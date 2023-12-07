6f1186277d9dd92d86f22f9495302089629a9fb5
Michael Filippo <6150073+Mogget24@users.noreply.github.com>
2023-12-07 16:00:51 +0100

    v1.0.5

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      "babel-preset-expo",
+      {
+        test: /@?(ui-kitten|eva-design).*\.(ts|js)x?$/,
+        loader: "babel-loader",
+      },
+    ],
   };
 };
