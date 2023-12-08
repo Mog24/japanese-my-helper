28c150f22ab699d59b391f4ad9b3d10cd905a547
Michael Filippo <6150073+Mogget24@users.noreply.github.com>
2023-12-08 18:48:42 +0100

    v.1.0.8 && working f'ing finally

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
 import * as eva from "@eva-design/eva";
-import {
-  ApplicationProvider,
-  IconRegistry,
-} from "@ui-kitten/components";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import { default as theme } from "./theme.json";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import { AppNavigator } from "./AppNavigator";
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
-    <>
+    <SafeAreaProvider>
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
         <AppNavigator />
         <StatusBar style="light" />
       </ApplicationProvider>
-    </>
+    </SafeAreaProvider>
   );
 }
